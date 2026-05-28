8f9e323cc77648ec0be26a90691e69b6429fc083
Mithulgm <mith514g@gmail.com>
2026-05-28 12:38:19 +0530

    Switched to PostgreSql

--- core/settings.py
+++ core/settings.py
@@ -138,10 +138,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     os.getenv('DB_NAME',     'hireai'),
+        'USER':     os.getenv('DB_USER',     'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST':     os.getenv('DB_HOST',     'localhost'),
+        'PORT':     os.getenv('DB_PORT',     '5432'),
     }
 }
 
